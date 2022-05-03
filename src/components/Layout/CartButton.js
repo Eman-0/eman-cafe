cd11343af9d5f601c32d43f8e10591a1a225d244
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2022-05-03 00:36:28 -0500

    Final touches to cart functionality

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,17 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const [btnIsHighlighted, setIsBtnHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
   const numCartItems = cartCtx.items.reduce((curNum, item) => {
     return curNum + item.amount;
-  }, 0)
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    setIsBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setIsBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
