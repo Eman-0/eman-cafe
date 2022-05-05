6d4a81d16122b83d0e9431bdd637269cce2c0d9d
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2022-05-04 19:40:46 -0500

    added aws dynamo support

--- src/components/Drinks/AvailableDrinks.js
+++ src/components/Drinks/AvailableDrinks.js
@@ -1,10 +1,34 @@
-import dummyDrinks from "./DummyDrinkData";
+import { useEffect, useState } from "react";
+
 import classes from "./AvailableDrinks.module.css";
 import Card from "../UI/Card";
 import DrinkItem from "./DrinkItem/DrinkItem";
 
 const AvailableDrinks = () => {
-  const drinkList = dummyDrinks.map((drink) => (
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    const fetchDrinks = async () => {
+      const response = await fetch('https://qo4m1rpial.execute-api.us-east-2.amazonaws.com/drinks');
+      const data = await response.json();
+      const loadedDrinks = [];
+
+      for (let i = 0; i < data.Count; i++) {
+        loadedDrinks.push({
+          id: data.Items[i].id,
+          name: data.Items[i].name,
+          description: data.Items[i].description,
+          price: data.Items[i].price,
+        });
+      }
+      
+      setDrinks(loadedDrinks);
+    }
+
+    fetchDrinks();
+  }, [])
+
+  const drinkList = drinks.map((drink) => (
     <DrinkItem
       id={drink.id}
       key={drink.id}
